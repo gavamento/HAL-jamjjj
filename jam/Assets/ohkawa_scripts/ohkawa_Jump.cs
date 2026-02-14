695bc7041eed562b350054402db0153bbdf9a1a7
TakutoOhkawa <ohkawa.takuto@gmail.com>
2026-02-14 14:35:34 +0900

    クリックしたオブジェクトを取得、プレイヤーのタグが付いてたらカウント増加

--- jam/Assets/ohkawa_scripts/ohkawa_Jump.cs
+++ jam/Assets/ohkawa_scripts/ohkawa_Jump.cs
@@ -6,6 +6,8 @@ public class JumpPowerScript : MonoBehaviour
     public string fallSceneName = "SampleTitle";    //ジャンプのシーンが終わったらこの名前のシーンに遷移する
     public KeyCode clickKey = KeyCode.Mouse0;       //ジャンプするためのキー
 
+    GameObject clickedGameObject;//クリックされたゲームオブジェクトを代入する変数
+
     public static int JumpPower = 0;                //ジャンプするためのパワー(仮)
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,9 +21,21 @@ public class JumpPowerScript : MonoBehaviour
     {
         if (Input.GetKeyDown(clickKey))
         {
-            //クリックでクリック回数を増加
-            JumpPower++;
-            Debug.Log("Jumpower: " + JumpPower);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                clickedGameObject = hit.collider.gameObject;
+                Debug.Log(clickedGameObject.name);//ゲームオブジェクトの名前を出力
+
+                if(clickedGameObject.tag == "Player")
+                {
+                    //クリックでクリック回数を増加
+                    JumpPower++;
+                    Debug.Log("Jumpower: " + JumpPower);
+                }
+            }
         }
 
         //if内の条件でシーン遷移(落ちるシーンに以降)
